bee94565d2537a112bfe1d6005f6baf0401090ab
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-09-21 12:30:53 -0700

    Working on adding soundcloud player widget

--- mixtape.js
+++ mixtape.js
@@ -6,3 +6,18 @@ var SC = require('node-soundcloud');
   Still working on understanding the authorization to get info from them.
 
 */
+
+// Initialize client
+SC.init({
+  id: 'your SoundCloud client ID',
+  secret: 'your SoundCloud client secret',
+  uri: 'your SoundCloud redirect URI'
+});
+ 
+// Connect user to authorize application
+var initOAuth = function(req, res) {
+  var url = SC.getConnectUrl();
+ 
+  res.writeHead(301, Location: url);
+  res.end();
+};
