3c6259e1c11a3ca0417de7fa44876d157723c046
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-09-21 12:00:58 -0700

    Start building requirements.

---
+++ mixtape.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var SC = require('node-soundcloud');
+
+/*I'm wanting to use this file to see how to get and post requests from SC. 
+
+  Still working on understanding the authorization to get info from them.
+
+*/
