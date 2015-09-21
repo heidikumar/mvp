d11dcc3077950cce1fd6a6fe55494b22e291db57
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-09-21 16:31:37 -0700

    This is the point at which I decided to ditch Angular and do my front-end in Backbone

---
+++ client/app.js
@@ -0,0 +1,13 @@
+var mixtapeApp = angular.modular('mixtapeApp', []);
+
+//no routing needed as this is a singlepage app
+
+mixtapeApp.controller('playlistController', function($scope){
+
+	$scope.title="Hello"
+
+});
+
+mixtapeApp.controller('searchController', function($scope){
+
+});
--- mixtape.js
+++ mixtape.js
@@ -12,38 +12,40 @@ app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 
 // Connect user to authorize application
-// var initOAuth = function(req, res) {
-//   var url = SC.getConnectUrl();
-//   res.writeHead(301, url);
-//   res.end();
-// };
+var initOAuth = function(req, res) {
+  var url = SC.getConnectUrl();
+  res.writeHead(301, url);
+  res.end();
+};
 
-// app.get('/', function(request, response){
-// 	setTokenAndInitSC();
-// 	console.log('app.get was called');
-// });
+app.get('/', function(request, response){		
+	console.log('requesting authorization');
+	initOAuth();
+	setTokenAndInitSC();
+	console.log('app.get was called');
+});
 
-// var setTokenAndInitSC = function(request, response){
-// 	var code = request.query.code;
+var setTokenAndInitSC = function(request, response){
+	var code = request.query.code;
 
-// 	return new Promise(function(response, reject){
-// 		SC.authorize(code, function(err, accessToken){
-// 			if (err) {reject(err);}
-// 			else {
-// 				resolve(accessToken);
-// 			}
-// 		})
-// 	})
-// 	.then(function(accessToken){
-// 		// Initialize connection with soundcloud
-// 		SC.init({
-// 		  id: '0e43e893359fed215845522dc5893aca',
-// 		  secret: '965993d4c4751cbcd470ddf46894dd21', 
-// 		  token: accessToken
-// 		  // uri: 'your SoundCloud redirect URI'
-// 		});
-// 	});
-// };
+	return new Promise(function(response, reject){
+		SC.authorize(code, function(err, accessToken){
+			if (err) {reject(err);}
+			else {
+				resolve(accessToken);
+			}
+		})
+	})
+	.then(function(accessToken){
+		// Initialize connection with soundcloud
+		SC.init({
+		  id: '0e43e893359fed215845522dc5893aca',
+		  secret: '965993d4c4751cbcd470ddf46894dd21', 
+		  token: accessToken
+		  // uri: 'your SoundCloud redirect URI'
+		});
+	});
+};
 
 app.listen(3000);
 console.log('App listening on port 3000');
